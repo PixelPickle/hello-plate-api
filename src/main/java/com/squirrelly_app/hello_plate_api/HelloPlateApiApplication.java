4ad7154c8d9ff7ae851280269b1f58b86b32ed22
KTBates <KTBates@basspro.com>
2025-07-22 14:10:17 -0500

    Initial Commit

---
+++ src/main/java/com/squirrelly_app/hello_plate_api/HelloPlateApiApplication.java
@@ -0,0 +1,13 @@
+package com.squirrelly_app.hello_plate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPlateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPlateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squirrelly_app/hello_plate_api/HelloPlateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirrelly_app.hello_plate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPlateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
